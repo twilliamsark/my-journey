60aa67298dfa96a112ee9eeeab458ccdce7f5e12
Todd <twilliamsark@gmail.com>
2024-03-20 16:45:32 -0500

    remove partially implemented filter

--- src/app/shared/services/journey.store.service.ts
+++ src/app/shared/services/journey.store.service.ts
@@ -24,31 +24,15 @@ export class JourneyStoryService {
 
   private state = signal<JourneyState>({
     journeys: [],
-    // journeys: [
-    //   {
-    //     id: '1',
-    //     createdOn: new Date().toISOString(),
-    //     title: '1st step',
-    //     note: 'See 2nd step',
-    //   },
-    //   {
-    //     id: '2',
-    //     createdOn: new Date().toISOString(),
-    //     title: '2nd step',
-    //     note: 'Something to journal on is what you have accomplished — goals you set or some that might have surprised you (like scuba diving), how you felt within and after those accomplishments and what else you have to achieve in life.',
-    //   },
-    // ],
     error: '',
   });
 
   journeys = computed(() => this.state().journeys);
   error = computed(() => this.state().error);
 
-  private filter$ = new Subject<{}>();
-  private journeys$: Observable<JourneyDocument[]> = combineLatest([
-    this.storage.db$,
-    this.filter$,
-  ]).pipe(switchMap(([db, filter]) => db.journeys.find(filter).$));
+  private journeys$: Observable<JourneyDocument[]> = this.storage.db$.pipe(
+    switchMap((db) => db.journeys.find().$),
+  );
 
   add$ = new Subject<AddJourney>();
   edit$ = new Subject<EditJourney>();
